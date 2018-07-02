8709d5a72f4731149ea18d86b1f6bb01158e6b6c
tmushayahama <tmushayahama@gmail.com>
2018-07-02 11:40:42 -0700

    readonly sequence. genes table and the gene page ui looks

--- src/app/main/content/pages/search/gene/gene.component.ts
+++ src/app/main/content/pages/search/gene/gene.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbsService } from '@agb.common/services/breadcrumbs/breadcrumbs.service';
 import { GeneService } from './gene.service';
 import { Gene } from './models/gene';
 import * as _ from 'lodash';
+import { MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-genes',
@@ -11,12 +13,17 @@ import * as _ from 'lodash';
 })
 
 export class GeneComponent implements OnInit {
+  @ViewChild(MatSort)
+  sort: MatSort;
 
   ptn: string;
   gene; Gene;
+  displayedColumns: string[] = ['proxy_org_long', 'proxy_gene'];
+  dataSource;
 
   constructor(
     private route: ActivatedRoute,
+    private breadcrumbsService: BreadcrumbsService,
     private geneService: GeneService) { }
 
   ngOnInit() {
@@ -24,7 +31,19 @@ export class GeneComponent implements OnInit {
       this.ptn = decodeURIComponent(params['ptn']);
       this.geneService.getGeneByPtn(this.ptn).then(response => {
         this.gene = this.geneService.gene;
+        this.dataSource = new MatTableDataSource(this.gene.proxy_genes);
+        this.dataSource.sort = this.sort;
+
+        this.breadcrumbsService.setCurrentBreadcrumbs([{
+          label: this.gene.ptn,
+          url: 'gene/' + this.gene.ptn
+        }]);
       });
     });
+
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
