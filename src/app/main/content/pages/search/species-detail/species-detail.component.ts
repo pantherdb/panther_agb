2221c39b7bbd87421a0784de8e56708911683041
Xiaosong Huang <xhuang310@gatech.edu>
2018-07-16 20:48:42 -0700

    update species detail

--- src/app/main/content/pages/search/species-detail/species-detail.component.ts
+++ src/app/main/content/pages/search/species-detail/species-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbsService } from '@agb.common/services/breadcrumbs/breadcrumbs.service';
+import { MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-species-detail',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./species-detail.component.scss']
 })
 export class SpeciesDetailComponent implements OnInit {
+  @ViewChild(MatSort)
+  sort: MatSort;
 
-  constructor() { }
+  species: string;
+  //displayedColumns: string[] = ['proxy_org_long', 'proxy_gene'];
+  //dataSource;
+
+  constructor(
+    private route: ActivatedRoute,
+    private breadcrumbsService: BreadcrumbsService) { }
 
   ngOnInit() {
-  }
+    this.route.params.subscribe((params) => {
+      this.species = decodeURIComponent(params['id']);
+      });
+    }
 
 }
