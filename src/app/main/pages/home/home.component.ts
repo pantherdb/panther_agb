90d72195b098dd3eebd34740f357b46c6e231bed
tmushayahama <tmushayahama@gmail.com>
2018-08-01 12:02:09 -0700

    added a sample home page

--- src/app/main/pages/home/home.component.ts
+++ src/app/main/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+
+import { noctuaAnimations } from '@noctua/animations';
 import { NoctuaConfigService } from '@noctua/services/config.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: noctuaAnimations]
 })
 export class HomeComponent implements OnInit {
 
