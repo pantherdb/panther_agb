addd14fa438b402c333cd0c64d8ccde0e48052f3
Xiaosong Huang <xhuang310@gatech.edu>
2018-08-09 11:46:12 -0700

    fix error

--- src/app/main/apps/species/species-list/species-flat-tree/species-flat-tree.component.ts
+++ src/app/main/apps/species/species-list/species-flat-tree/species-flat-tree.component.ts
@@ -51,7 +51,7 @@ export class SpeciesFlatTreeComponent implements OnInit {
 
     this.router.navigate([`species/genes`, {
       outlets: {
-        'list': ['genes', `${this.selectedSpecies.short_name}`, 'default']
+        'list': ['genes', `${this.selectedSpecies.short_name}`, 'default species']
       }
     }]);
 
