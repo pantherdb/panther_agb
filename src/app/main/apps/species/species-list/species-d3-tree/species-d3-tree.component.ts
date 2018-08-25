9ce9075015c0023bfefbef527c8b84b161509020
Xiaosong Huang <xhuang310@gatech.edu>
2018-08-25 10:57:57 -0700

    add search to d3 tree view

--- src/app/main/apps/species/species-list/species-d3-tree/species-d3-tree.component.ts
+++ src/app/main/apps/species/species-list/species-d3-tree/species-d3-tree.component.ts
@@ -2,16 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import * as d3 from "d3";
 import * as $ from "jquery";
 
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
 @Component({
   selector: 'app-species-d3-tree',
   templateUrl: './species-d3-tree.component.html',
   styleUrls: ['./species-d3-tree.component.css']
 })
 export class SpeciesD3TreeComponent implements OnInit {
+  
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
 
   constructor() { }
 
   ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+    
     // Get JSON data
     const treeJSON = d3.json("assets/data/species-nodes.json", function (error, treeData) {
       console.log(treeData);
@@ -588,6 +601,12 @@ export class SpeciesD3TreeComponent implements OnInit {
       });
     });
 
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 
 }
