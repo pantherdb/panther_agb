ca06caa800d3ff84bf5580f4b860c1d14b17e5d3
Xiaosong Huang <xhuang310@gatech.edu>
2018-12-04 17:00:03 -0800

    add https api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //apiUrl: 'http://68.181.125.145:3000'
-  apiUrl: 'http://159.89.146.180:3001'
+  apiUrl: 'https://159.89.146.180:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://68.181.125.145:3000'
-  apiUrl: 'http://159.89.146.180:3001'
+  apiUrl: 'https://159.89.146.180:3000'
 };
 
 /*
